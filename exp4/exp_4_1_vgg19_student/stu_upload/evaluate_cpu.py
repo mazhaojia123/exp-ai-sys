6e7e313e4594985a06f7fd9166d1ee6a0852682c
mazhaojia123 <2832026690@qq.com>
2024-04-22 19:30:30 +0800

    [exp4] run_cpu failed.

--- exp4/exp_4_1_vgg19_student/stu_upload/evaluate_cpu.py
+++ exp4/exp_4_1_vgg19_student/stu_upload/evaluate_cpu.py
@@ -4,6 +4,8 @@ import torch.nn as nn
 import time
 from PIL import Image
 from torchvision import transforms
+import scipy.misc
+import numpy as np 
 
 os.putenv('MLU_VISIBLE_DEVICES','')
 cfgs = [64,'R', 64,'R', 'M', 128,'R', 128,'R', 'M',
@@ -29,56 +31,63 @@ def vgg19():
     for i, layer_name in enumerate(layers):
         if layer_name.startswith('conv'):
             # TODO: 在时序容器中传入卷积运算
-            ________________________________________________
+            layer_container.append(nn.Conv2d(in_channels, cfgs[i], kernel_size=(3, 3), padding=(1, 1)))
+            in_channels = cfgs[i]
         elif layer_name.startswith('relu'):
             # TODO: 在时序容器中执行ReLU计算
-            ________________________________________________
+            layer_container.append(nn.ReLU())
         elif layer_name.startswith('pool'):
             # TODO: 在时序容器中执行maxpool计算
-            ________________________________________________
+            layer_container.append(nn.MaxPool2d(kernel_size=2, stride=2))
         elif layer_name == 'flatten':
-            # TODO: 在时序容器中执行flatten计算
-            ________________________________________________
+            # TODO: 在时序容器中执行flatten计算 
+            layer_container.append(nn.Flatten())
         elif layer_name == 'fc6':
             # TODO: 在时序容器中执行全连接层计算
-            ________________________________________________
+            layer_container.append(nn.Linear(512*7*7, 4096))
         elif layer_name == 'fc7':
             # TODO: 在时序容器中执行全连接层计算
-            ________________________________________________
+            layer_container.append(nn.Linear(4096, 4096))
         elif layer_name == 'fc8':
             # TODO: 在时序容器中执行全连接层计算
-            ________________________________________________
+            layer_container.append(nn.Linear(4096, num_classes))
         elif layer_name == 'softmax':
             # TODO: 在时序容器中执行Softmax计算
-            ________________________________________________
+            layer_container.append(nn.Softmax(dim=1))
     return layer_container
 
 
 def load_image(path):
     #TODO: 使用 Image.open模块读入输入图像，并返回形状为（1,244,244,3）的数组 image
-    ________________________________________________
+    # use Image.open to read image
+    image = Image.open(path)
+    image = image.resize((224, 224))
+    # image = np.array(image).astype(np.float32)
+    
     transform = transforms.Compose([transforms.Resize(256),
                                   transforms.CenterCrop(224),
                                   transforms.ToTensor(),
                                   transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                                       std=[0.229, 0.224, 0.225])])
     #TODO: 对图像调用transform函数进行预处理
-    ________________________________________________
+    # preprocess image
+    image = transform(image)
     #TODO: 对tensor的第0维进行扩展
-    ________________________________________________
+    image = image.reshape([1]+list(image.shape))
     return image
 
 if __name__ == '__main__':
     input_image = load_image(IMAGE_PATH)
     #TODO: 生成VGG19网络模型并保存在net中
-    ________________________________________________
+    net = vgg19()
+    print(net)
     #TODO: 加载网络参数到net中
-    ________________________________________________
+    net.load_state_dict(torch.load(VGG_PATH))
     #TODO: 模型进入推理模式
-    ________________________________________________
+    net.eval()
     st = time.time()
     # TODO: 计算 net,得到prob
-    ________________________________________________
+    prob = net(input_image)
     print("cpu infer time:{:.3f} s".format(time.time()-st))
     with open('./labels/imagenet_classes.txt') as f:
         classes = [line.strip() for line in f.readlines()]
--- exp4/exp_4_1_vgg19_student/stu_upload/generate_pth.py
+++ exp4/exp_4_1_vgg19_student/stu_upload/generate_pth.py
@@ -28,47 +28,67 @@ def vgg19():
     for i, layer_name in enumerate(layers):
         if layer_name.startswith('conv'):
             # TODO: 在时序容器中传入卷积运算
-            ________________________________________________
+            layer_container.append(nn.Conv2d(in_channels, cfgs[i], kernel_size=(3, 3), padding=(1, 1)))
+            in_channels = cfgs[i]
         elif layer_name.startswith('relu'):
             # TODO: 在时序容器中执行ReLU计算
-            ________________________________________________
+            layer_container.append(nn.ReLU())
         elif layer_name.startswith('pool'):
             # TODO: 在时序容器中执行maxpool计算
-            ________________________________________________
+            layer_container.append(nn.MaxPool2d(kernel_size=2, stride=2))
         elif layer_name == 'flatten':
-            # TODO: 在时序容器中执行flatten计算
-            ________________________________________________
+            # TODO: 在时序容器中执行flatten计算 
+            layer_container.append(nn.Flatten())
         elif layer_name == 'fc6':
             # TODO: 在时序容器中执行全连接层计算
-            ________________________________________________
+            layer_container.append(nn.Linear(512*7*7, 4096))
         elif layer_name == 'fc7':
             # TODO: 在时序容器中执行全连接层计算
-            ________________________________________________
+            layer_container.append(nn.Linear(4096, 4096))
         elif layer_name == 'fc8':
             # TODO: 在时序容器中执行全连接层计算
-            ________________________________________________
+            layer_container.append(nn.Linear(4096, num_classes))
         elif layer_name == 'softmax':
             # TODO: 在时序容器中执行Softmax计算
-            ________________________________________________
+            layer_container.append(nn.Softmax(dim=1))
     return layer_container
 
 
 if __name__ == '__main__':
     #TODO:使用scipy加载.mat格式的VGG19模型
-    ________________________________________________
+    datas = scipy.io.loadmat(VGG_PATH)['layers'][0]
 
     model = vgg19()
     new_state_dict = OrderedDict()
     for i, param_name in enumerate(model.state_dict()):
         name = param_name.split('.')
-        if name[-1] == 'weight':
-            new_state_dict[param_name] = torch.from_numpy(datas[str(i)]).float()
+        index = int(name[0])
+        if index < 37:
+            if name[-1] == 'weight':
+                new_state_dict[param_name] = torch.from_numpy(datas[index][0][0][0][0][0]).float()
+            else:
+                new_state_dict[param_name] = torch.from_numpy(datas[index][0][0][0][0][1][0]).float()
         else:
-            new_state_dict[param_name] = torch.from_numpy(datas[str(i)][0]).float()
+            if name[-1] == 'weight':
+                new_state_dict[param_name] = torch.from_numpy(datas[index-1][0][0][0][0][0]).float()
+            else:
+                new_state_dict[param_name] = torch.from_numpy(datas[index-1][0][0][0][0][1][0]).float()
     #TODO:加载网络参数到model
-    ________________________________________________
+    for i, param_name in enumerate(new_state_dict):
+        name = param_name.split('.')
+        if name[-1] == 'weight':
+            index = int(name[0])
+            if index < 37:  # conv: []
+                new_state_dict[param_name] = new_state_dict[param_name].permute(3, 2, 0, 1)
+            else:           # fc
+                sz = new_state_dict[param_name].size()
+                shp = [sz[0]*sz[1]*sz[2], sz[3]]
+                tmp = torch.reshape(new_state_dict[param_name], (shp[0], shp[1]))
+                new_state_dict[param_name] = tmp.permute(1, 0)
+
+    model.load_state_dict(new_state_dict)
     print("*** Start Saving pth ***")
     #TODO:保存模型的参数到models/vgg19.pth
-    ________________________________________________
+    torch.save(model.state_dict(), 'models/vgg19.pth')
     print('Saving pth  PASS.')
     
